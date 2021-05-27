164b6bed24b0fe2095aa25b0627f975f5170dd34
Simon Louvet <simon.louvet.zen@gmail.com>
2021-05-27 16:42:26 +0200

    title

--- frontend/src/resources/News/NewsShow.js
+++ frontend/src/resources/News/NewsShow.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Show } from '@semapps/archipelago-layout';
+import {Typography, Box, makeStyles} from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+import { MainList, Show } from '@semapps/archipelago-layout';
 import MarkdownField from "../../markdown/MarkdownField";
 import NewsTitle from './NewsTitle';
 
