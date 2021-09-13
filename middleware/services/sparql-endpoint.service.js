83eae0a49c7acb0b2fda3aeb2a76f52f3179fdef
BastienSig <bastien.siguier@gmail.com>
2021-09-13 09:58:31 +0200

    delete double module.exp sparqlendservice

--- middleware/services/sparql-endpoint.service.js
+++ middleware/services/sparql-endpoint.service.js
@@ -1,7 +1,5 @@
 const { SparqlEndpointService } = require('@semapps/sparql-endpoint');
 
-module.exports = SparqlEndpointService;
-
 module.exports = {
   mixins: [SparqlEndpointService],
   settings: {
