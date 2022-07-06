2795a345a23a4fda1daffc0650f72ecc681e445d
BastienSig <bastien.siguier@gmail.com>
2022-07-06 18:46:21 +0200

    set up backup

--- middleware/services/backup.service.js
+++ middleware/services/backup.service.js
@@ -11,47 +11,16 @@ module.exports = {
         uploads: path.resolve(__dirname, '../uploads')
       }
     },
-    cronJob: {
-      time: '0 4 * * *', // Every night at 4am
-      timeZone: 'Europe/Paris'
+    copyMethod: 'ftp',
+    remoteServer: {
+      user: CONFIG.BACKUP_SERVER_USER,
+      password: CONFIG.BACKUP_SERVER_PASSWORD,
+      host: CONFIG.BACKUP_SERVER_HOST,
+      path: CONFIG.BACKUP_SERVER_PATH
     },
-    actions: {
-      syncWithRemoteServer(ctx) {
-        return new Promise(async (resolve, reject) => {
-          try {
-            let sftp = new Client();
-  
-            await sftp.connect({
-              host: process.env.SEMAPPS_FTP_HOST,
-              port: process.env.SEMAPPS_FTP_PORT,
-              username: process.env.SEMAPPS_FTP_USER,
-              password: process.env.SEMAPPS_FTP_PASSWORD
-            });
-  
-            fs.readdir(ctx.params.path, async function (err, files) {
-              if (err) {
-                  return console.log('Unable to scan directory: ' + err);
-                  reject('Unable to scan directory: ' + err.message)
-              }
-              const now = Date.now();
-              files = files.map(function (fileName) {
-                return {
-                  name: fileName,
-                  diff: (now - fs.statSync(ctx.params.path + '/' + fileName).mtime) / 1000
-                };
-              }).filter(f=>f.diff<60)
-  
-              for (var file of files) {
-                await sftp.put(ctx.params.path + '/' +file.name, process.env.SEMAPPS_FTP_PATH +file.name);
-              }
-  
-              resolve()
-            });
-          } catch (e) {
-            reject (e)
-          }
-        });
-      }
+    cronJob: {
+      time: '48 18 * * *', // Every night at 4am
+      timeZone: 'Europe/Paris'
     }
   }
 };
