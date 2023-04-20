14dc7046b0bde52da9287c801eaf9bf56383dc55
bastien.siguier <bastien.siguier@gmail.com>
2023-04-20 11:38:15 +0200

    remove useless param custommapfield

--- frontend/src/resources/Task/CustomMapField.js
+++ frontend/src/resources/Task/CustomMapField.js
@@ -3,7 +3,7 @@ import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import { Box } from '@material-ui/core';
 import { useRecordContext } from 'react-admin';
 
-const CustomMapField = ({ latitude, longitude, height, addLabel, ...rest }) => {
+const CustomMapField = ({ height, addLabel, ...rest }) => {
   const record = useRecordContext();
   const position = [record["prats:lat"], record["prats:lng"]];
   if (!position[0] || !position[1]) return null;
