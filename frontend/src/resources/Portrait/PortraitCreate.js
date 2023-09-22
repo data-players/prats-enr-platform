d91090f9b0a9a493477034e4b93238cc11bb7883
bastien.siguier <bastien.siguier@gmail.com>
2023-09-22 14:55:54 +0200

    adding create

--- frontend/src/resources/Portrait/PortraitCreate.js
+++ frontend/src/resources/Portrait/PortraitCreate.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { SimpleForm, TextInput } from 'react-admin';
+import { SimpleForm, TextInput, required } from 'react-admin';
 import { Create } from '@semapps/archipelago-layout';
 
 const PortraitCreate = props => (
   <Create {...props}>
     <SimpleForm>
-      <TextInput source="pair:label" fullWidth />
+      <TextInput source="pair:label" validate={required()} fullWidth />
+      <TextInput label="Ordre dans la liste : choisir un chiffre, 1 sera le premier à afficher" source='prats:listOrder' defaultValue={"999999"} validate={required()} fullWidth />
     </SimpleForm>
   </Create>
 );
